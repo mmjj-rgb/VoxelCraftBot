01ad9df4d662aa1eaeb8e0d3435ed5b5e3c0425f
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 10:55:57 +0200

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,14 +1,8 @@
-exports.run = (client, message, args) => {
-    const { get } = require('snekfetch')
+exports.run = async (client, message, args) => {
+    const fetch = require('node-fetch')
     const Discord = require('discord.js')
     message.delete()
-    try {
-        get('https://dog.ceo/api/breeds/image/random').then(res => {
-            const embed = new Discord.MessageEmbed()
-            .setImage(res.body.message)
-            return message.channel.send({embed});
-        });
-    } catch(err) {
-        return message.channel.send(err.stack);
-    }
-}
+    const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(reponse => reponse.json());
+    const embed = new Discord.MessageEmbed()
+    .setImage(message) 
+} 
