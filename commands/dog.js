6fca0a21366a28baf7f0d6214b2961daac718b85
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 15:52:03 +0200

    El

--- commands/dog.js
+++ commands/dog.js
@@ -2,8 +2,8 @@ exports.run = async (client, message, args) => {
     const fetch = require('node-fetch')
     const Discord = require('discord.js')
     message.delete()
-    const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(reponse => reponse.json());
+    const { messages } = await fetch('https://dog.ceo/api/breeds/image/random').then(reponse => reponse.json());
     const embed = new Discord.MessageEmbed()
-    .setImage(message) 
+    .setImage(messages) 
     message.channel.send(embed)
 } 
