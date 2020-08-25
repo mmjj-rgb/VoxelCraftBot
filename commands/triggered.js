5c8e77743e05b0f93173ec064cbc526ae8acacc8
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 17:07:45 +0200

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -1,1 +1,3 @@
-exports.run (message, args 
+exports.run (client, message, args) => {
+    message.channel.send(`https://some-random-api.ml/canvas/YouTube-comment?avatar=${message.author.avatarUrl}&username=${message.author.username}&comment=args`) 
+} 
