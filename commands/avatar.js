0566f7b1765d39293ae5d044155e86ffb4da4db6
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 22:19:33 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,11 +1,12 @@
 exports.run = async (client, message, args) => {
     const Discord = require('discord.js')
     const text = args.join(" ") 
+    const img = message.author.avatarUrl
     message.channel.send({embed: {
         title: "STRONA", 
         description: `[POBIERZ](${message.author.avatarUrl} )`, 
         image: {
-		url: `${message.author.avatarUrl}`,
+		url: img,
 	},
       }
    })
