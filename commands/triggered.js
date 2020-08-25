f60ae17f528513751014a89c20942cb59c341734
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 21:44:11 +0200

    Delete triggered.js

--- commands/triggered.js
+++
@@ -1,4 +0,0 @@
-exports.run (client, message, args) => {
-    let text = args.join(" ")
-    message.channel.send(`https://some-random-api.ml/canvas/YouTube-comment?avatar=${message.author.avatarUrl}&username=${message.author.username}&comment=${text}`) 
-} 
