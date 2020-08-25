70faf38826a5bb5e3168073220f270cdc9ea42eb
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-08-25 22:07:58 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
     const Discord = require('discord.js')
     const text = args.join(" ") 
-    const embed = new Discord.MessageEmbed 
+    const embed = new Discord.MessageEmbed()
     .setTitle(`Avatar użytkownika ${text}`) 
     .setDescription('Pobierz')
     .setUrl(`${message.author.avatarUrl}`)
