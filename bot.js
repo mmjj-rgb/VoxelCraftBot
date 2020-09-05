c452387fc36b752932e134b1b36462b5f5c8ab64
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-05 22:16:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,24 +10,6 @@ const { get } = require('snekfetch')
 
 const Enmap = require("enmap");
 
-const antiSpam = new AntiSpam({
-    warnThreshold: 4, // Amount of messages sent in a row that will cause a warning.
-    kickThreshold: 15, // Amount of messages sent in a row that will cause a ban.
-    banThreshold: 25, // Amount of messages sent in a row that will cause a ban.
-    maxInterval: 10000, // Amount of time (in milliseconds) in which messages are considered spam.
-    warnMessage: '{@user}, Nie spam!', // Message that will be sent in chat upon warning a user.
-    kickMessage: '**{user_tag}** Został wyrzucony za spamienie.', // Message that will be sent in chat upon kicking a user.
-    banMessage: '**{user_tag}** Został zbanowany za spamienie.', // Message that will be sent in chat upon banning a user.
-    maxDuplicatesWarning: 4, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesKick: 7, // Amount of duplicate messages that trigger a warning.
-    maxDuplicatesBan: 10, // Amount of duplicate messages that trigger a warning.
-    exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
-    ignoreBots: true, // Ignore bot messages.
-    verbose: true, // Extended Logs from module.
-    ignoredUsers: [], // Array of User IDs that get ignored.
-    // And many more options... See the documentation.
-});
-
 client.commands = new Discord.Collection();
 
 client.aliases = new Discord.Collection();
