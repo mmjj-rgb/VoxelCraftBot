d9f688a5985164d8eaaca44f4f5f04960017f8db
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-05 19:38:10 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,1 +1,35 @@
+exports.run = (client, message, args) => {
+    const ms = require('ms');
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tomute) return message.reply("Nie znaleziono użytkownika.");
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Nie mam uprawnień do wyciszenia tego użytkownika!");
+    let muterole = message.guild.cache.roles.find(`name`, "muted");
+    if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "muted",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  //end of create role
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("Nie podałeś czasu!");
 
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> został odmutowany!`);
+  }, ms(mutetime));
+}
