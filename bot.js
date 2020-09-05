d1305a5ded5638bd50ce973060bbc3129bdef2fb
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-05 22:04:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,24 @@ const { get } = require('snekfetch')
 
 const Enmap = require("enmap");
 
+const antiSpam = new AntiSpam({
+    warnThreshold: 5, // Amount of messages sent in a row that will cause a warning.
+    kickThreshold: 15, // Amount of messages sent in a row that will cause a ban.
+    banThreshold: 25, // Amount of messages sent in a row that will cause a ban.
+    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+    warnMessage: '{@user}, Nie spam!', // Message that will be sent in chat upon warning a user.
+    kickMessage: '**{user_tag}** Został wyrzucony za spamienie.', // Message that will be sent in chat upon kicking a user.
+    banMessage: '**{user_tag}** Został zbanowany za spamienie.', // Message that will be sent in chat upon banning a user.
+    maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+    maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
+    exemptPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
+    ignoreBots: true, // Ignore bot messages.
+    verbose: true, // Extended Logs from module.
+    ignoredUsers: [], // Array of User IDs that get ignored.
+    // And many more options... See the documentation.
+});
+
 client.commands = new Discord.Collection();
 
 client.aliases = new Discord.Collection();
@@ -39,7 +57,9 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.on('message', async message => {
+client.on('message', (message) => antiSpam.message(message)); 
+
+client.on('message', async message =>  {
     if (message.author.bot || message.author === client.user) return;
     let guild = client.guilds.cache.get('711234725955633173')
 	  var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
