f6d6aa65658723549108f9ff9090fb883a31d8d8
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-05 22:18:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,8 +41,6 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.on('message', (message) => antiSpam.message(message)); 
-
 client.on('message', async message =>  {
     if (message.author.bot || message.author === client.user) return;
     let guild = client.guilds.cache.get('711234725955633173')
