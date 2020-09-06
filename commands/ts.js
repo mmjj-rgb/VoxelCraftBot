45547ad44e45d2f11b5259abca30d39b49fa9775
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-06 20:40:16 +0200

    Update ts.js

--- commands/ts.js
+++ commands/ts.js
@@ -19,35 +19,4 @@ exports.run = async (client, message, args) => {
         );
         sent.react('🎫');
         settings.set(`${message.guild.id}-zgloszenie`, sent.id);
-        
-client.on('messageReactionAdd', async (reaction, user) => {
-    if(user.partial) await user.fetch();
-    if(reaction.partial) await reaction.fetch();
-    if(reaction.message.partial) await reaction.message.fetch();
-
-    if(user.bot) return;
-
-    let ticketid = await settings.get(`${reaction.message.guild.id}-ticket`);
-
-    if(!ticketid) return;
-
-    if(reaction.message.id == ticketid && reaction.emoji.name == '🎫') {
-        reaction.users.remove(user);
-
-        reaction.message.guild.channels.create(`ticket-${user.username}`, {
-            permissionOverwrites: [
-                {
-                    id: user.id,
-                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL"]
-                },
-                {
-                    id: reaction.message.guild.roles.everyone,
-                    deny: ["VIEW_CHANNEL"]
-                }
-            ],
-            type: 'text'
-        }).then(async channel => {
-            channel.send(`<@${user.id}>`, new Discord.MessageEmbed().setTitle("Welcome to your ticket!").setDescription("We will be with you shortly").setColor("00ff00"))
-        })}
-    })
-  }
+}
