865385f3328775f1859fc401beb04dc777c5e33b
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-06 20:28:37 +0200

    Update ts.js

--- commands/ts.js
+++ commands/ts.js
@@ -13,9 +13,8 @@ exports.run = async (client, message, args) => {
       if(!channel) return message.reply("Usage: `!ts #kanał`");
         let sent = await channel.send(new Discord.MessageEmbed()
             .setTitle("Zgłoszenia")
-            .setDescription("Znalazłeś/aś błąd lub chcesz się odwołać od bana lub złożyć skargę na gracza?
-            Stwórz kanał na zgłoszenie poprzez kliknięcie w reakcje :envelope_with_arrow:")
-            .addField("**Uwaga:** Jeśli na Discordzie posiadasz inny nick niż w Minecraft, to masz obowiązek podać w zgłoszeniu swój nick!")
+            .setDescription("Znalazłeś/aś błąd lub chcesz się odwołać od bana lub złożyć skargę na gracza? Stwórz kanał na zgłoszenie poprzez kliknięcie w reakcje :envelope_with_arrow:")
+            .addField("**Uwaga:** Jeśli na Discordzie posiadasz inny nick niż w Minecraft, to masz obowiązek podać w zgłoszeniu swój nick!", "\u200B")
             .setColor("00e1ff")
         );
         sent.react('🎫');
