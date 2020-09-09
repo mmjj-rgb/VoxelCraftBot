3f5b79665b566a75c4b182c909781ffa60568e51
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-09 11:24:15 +0200

    Update elprimo.js

--- commands/elprimo.js
+++ commands/elprimo.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send('https://cdn.discordapp.com/attachments/743837116856795236/753181862209388614/received_768775117220762.jpeg')
+    message.delete()
+    message.author.send('https://cdn.discordapp.com/attachments/743837116856795236/753181862209388614/received_768775117220762.jpeg')
 } 
