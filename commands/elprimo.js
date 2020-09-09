0303100318464212f9f8ca39967dffd0aac44686
mmjj-rgb <60490029+mmjj-rgb@users.noreply.github.com>
2020-09-09 11:21:37 +0200

    Create elprimo.js

---
+++ commands/elprimo.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send('https://cdn.discordapp.com/attachments/743837116856795236/753181862209388614/received_768775117220762.jpeg')
+} 
